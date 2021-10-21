51bf233cf7f3337cb619327f82b869969dcd08dc
Guilherme Corrêa Teixeira <gui.cor.tei@gmail.com>
2021-10-21 14:33:50 -0300

    serving static files /tmp -> localhost:3333/files

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 import './database';
+import multerConfig from './config/multerConfig';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(multerConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
