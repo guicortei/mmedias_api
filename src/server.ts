a07307286ce252618c3c3a2ea9baabd1f0047d63
Guilherme Corrêa Teixeira <gui.cor.tei@gmail.com>
2021-10-25 15:15:21 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes';
 import './database';
 import multerConfig from './config/multerConfig';
@@ -8,6 +9,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(multerConfig.directory));
 app.use(routes);
