04ebc3f7686abb3327956068c89e2a1bdfff906f
Guilherme Corrêa Teixeira <gui.cor.tei@gmail.com>
2021-12-18 14:30:42 -0300

    mmedias_api first commit8

--- src/routes/root.routes.ts
+++ src/routes/root.routes.ts
@@ -3,7 +3,8 @@ import { Router } from 'express';
 const rootRoutes = Router();
 
 rootRoutes.get('/', async (request, response) => {
-  return response.json({ message: 'Hello World!' });
+  const req = request.body;
+  return response.json(req);
 });
 
 export default rootRoutes;
