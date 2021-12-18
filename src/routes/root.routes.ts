8bed56eb0d9f954e01da166584cecb7a43da0a00
Guilherme Corrêa Teixeira <gui.cor.tei@gmail.com>
2021-12-18 17:58:33 -0300

    mmedias_api first commit24

--- src/routes/root.routes.ts
+++ src/routes/root.routes.ts
@@ -2,14 +2,14 @@ import { Router } from 'express';
 
 const rootRoutes = Router();
 
-rootRoutes.get('/:parametros', async (request, response) => {
+rootRoutes.get('/', async (request, response) => {
   let req = request.body;
   console.log(request.body);
   console.log(request.query);
   console.log(request.params);
-  if (Object.keys(req).length === 0) {
-    req = request.body;
-  }
+  // if (Object.keys(req).length === 0) {
+  //   req = request.body;
+  // }
   if (Object.keys(req).length === 0) {
     req = { vazio: 'estou' };
   }
