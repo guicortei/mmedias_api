3396ccc424dd749d52af54046e375cf51d8cc834
Guilherme Corrêa Teixeira <gui.cor.tei@gmail.com>
2021-12-18 16:51:12 -0300

    mmedias_api first commit19

--- src/routes/root.routes.ts
+++ src/routes/root.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 const rootRoutes = Router();
 
-rootRoutes.get('/', async (request, response) => {
+rootRoutes.get('/:params', async (request, response) => {
   let req = request.body;
   console.log(req);
   if (Object.keys(req).length === 0) {
