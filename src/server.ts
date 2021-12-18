b0f47cef94b4faa42f126da224e98ecd698405a4
Guilherme Corrêa Teixeira <gui.cor.tei@gmail.com>
2021-12-18 15:08:30 -0300

    mmedias_api first commit11

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(
     origin: 'http://localhost:3000',
   }),
 );
-app.use(express.json());
+// app.use(express.json());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
