35f17e87a1f1aa3ac43532624202e48392c244d8
Guilherme Corrêa Teixeira <gui.cor.tei@gmail.com>
2021-12-18 17:46:20 -0300

    mmedias_api first commit22

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,8 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   app.use(cors());
   next();
 });
